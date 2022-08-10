72974cbd2b18ea64ed3d992041b6a922818a3584
ganxby <70334053+ganxby@users.noreply.github.com>
2022-08-10 11:30:22 +0200

    Update client.py

--- client.py
+++ client.py
@@ -5,11 +5,6 @@ import json
 import asyncio
 
 
-f = open('config.json', 'r')
-parsed_file = json.load(f)
-
-api = ManagerAPI(login=parsed_file['login'], password=parsed_file['passwd'])
-
 menu = 'Menu:\n' \
        '1) Create group\n' \
        '2) Mailing to one group\n' \
@@ -22,53 +17,67 @@ menu = 'Menu:\n' \
 
 print(menu)
 
-
 while True:
-    input_data = input('--> ')
+    try:
+        input_data = input('--> ')
 
-    if input_data == 'menu':
-        print('\n' + menu)
-
-    if input_data == 'q':
-        sys.exit()
-
-    if input_data == '1':
-        f = open('data.json', 'r')
+        f = open('config.json', 'r')
         parsed_file = json.load(f)
-        f.close()
 
-        request = api.group_create(parsed_file['data'])
-        corout = asyncio.run(request)
-        print(corout, '\n')
+        api = ManagerAPI(login=parsed_file['login'], password=parsed_file['passwd'])
 
-    if input_data == '2':
-        f = open('data.json', 'r')
-        parsed_file = json.load(f)
-        f.close()
+        if input_data == 'menu':
+            print('\n' + menu)
 
-        request = api.mailing_to_one_group(parsed_file['data'])
-        corout = asyncio.run(request)
-        print(corout, '\n')
+        if input_data == 'q':
+            sys.exit()
 
-    if input_data == '3':
-        f = open('data.json', 'r')
-        parsed_file = json.load(f)
-        f.close()
+        if input_data == '1':
+            f = open('data.json', 'r')
+            parsed_file = json.load(f)
+            f.close()
 
-        request = api.multiple_mailing(parsed_file['data'])
-        corout = asyncio.run(request)
-        print(corout, '\n')
+            request = api.group_create(parsed_file['data'])
+            corout = asyncio.run(request)
+            print(corout, '\n')
 
-    if input_data == '4':
-        request = api.subscribers_info()
-        corout = asyncio.run(request)
-        print(corout, '\n')
+        if input_data == '2':
+            f = open('data.json', 'r')
+            parsed_file = json.load(f)
+            f.close()
 
-    if input_data == '5':
-        f = open('data.json', 'r')
-        parsed_file = json.load(f)
-        f.close()
+            request = api.mailing_to_one_group(parsed_file['data'])
+            corout = asyncio.run(request)
+            print(corout, '\n')
 
-        request = api.filter_by_notification_type(parsed_file['data'])
-        corout = asyncio.run(request)
-        print(corout, '\n')
+        if input_data == '3':
+            f = open('data.json', 'r')
+            parsed_file = json.load(f)
+            f.close()
+
+            request = api.multiple_mailing(parsed_file['data'])
+            corout = asyncio.run(request)
+            print(corout, '\n')
+
+        if input_data == '4':
+            request = api.subscribers_info()
+            corout = asyncio.run(request)
+            print(corout, '\n')
+
+        if input_data == '5':
+            f = open('data.json', 'r')
+            parsed_file = json.load(f)
+            f.close()
+
+            request = api.filter_by_notification_type(parsed_file['data'])
+            corout = asyncio.run(request)
+            print(corout, '\n')
+
+    except FileNotFoundError:
+        print('File "data.json" or "config.json" not found')
+
+    except KeyError:
+        print('Incorrect data in "config.json" or "data.json"')
+
+    except json.decoder.JSONDecodeError:
+        print('Incorrect data in "config.json" or "data.json"')
